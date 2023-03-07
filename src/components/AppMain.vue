40c329dad1497c3e9efa77ca159e5c7548b6f11a
Shelbot77 <alessiodeangelis021@gmail.com>
2023-03-07 16:20:56 +0100

    test Api call in Vue (OK!)

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,11 +1,46 @@
 <script>
+import axios from 'axios';
 export default {
     name: 'AppMain',
     //components
     data() {
         return {
 
+            booksList: [],
+            apiUrl: 'http://127.0.0.1:8000/api/books',
+
         }
+    },
+    methods: {
+        getBooks() {
+            const self = this;
+
+            axios.get(this.apiUrl, {
+                params: {
+
+                }
+            })
+                .then(function (response) {
+                    console.log(response.data.results.data);
+                    self.booksList = response.data.results.data;
+
+
+                })
+                .catch(function (error) {
+                    console.log(error);
+                })
+                .then(function () {
+                    // always executed
+                });
+        },
+
+
+
+
+    },
+    created() {
+        this.getBooks()
+
     }
 
 }
