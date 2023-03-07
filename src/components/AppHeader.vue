15a5b82f3a1f510698b2559bd005e7cc749968d5
Shelbot77 <alessiodeangelis021@gmail.com>
2023-03-07 16:48:39 +0100

    Bootstrap navbar import

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -12,6 +12,30 @@ export default {
 
 </script>
 
-<template></template>
+<template>
+    <nav class="navbar navbar-expand-lg bg-light">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">Team5 Books</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown"
+                aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNavDropdown">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Books</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">About Us</a>
+                    </li>
 
-<style scoped></style>
+                </ul>
+            </div>
+        </div>
+    </nav>
+</template>
+
+<style scoped lang="scss"></style>
