cc66ed49e973051f4be7a8901e0d447cbbd21595
Shelbot77 <alessiodeangelis021@gmail.com>
2023-03-07 16:27:36 +0100

    router.js setup commit

---
+++ src/router.js
@@ -0,0 +1,31 @@
+import { createRouter, createWebHistory } from 'vue-router';
+
+import HomePage from './pages/HomePage.vue';
+import AboutUs from './pages/AboutUs.vue';
+import ProjectIndex from './pages/ProjectIndex.vue';
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes: [
+        {
+            path: '/',
+            name: '',
+            component: 
+
+        },
+        {
+            path: '',
+            name: '',
+            component: 
+
+        },
+        {
+            path: '/',
+            name: '',
+            component: 
+
+        },
+    ]
+});
+
+export { router };
